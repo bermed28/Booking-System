dfcc4a793b60771400677830831c9f94f7723033
Gabriel Diaz Morro <gabriel.diaz34@upr.edu>
2021-11-12 22:36:30 -0400

    Fixed user schedule crud operations due to changes to DB

--- controller/user_schedule.py
+++ controller/user_schedule.py
@@ -6,16 +6,14 @@ class BaseUserSchedule:
     def build_map_dict(self, row):
         result = {}
         result['usid'] = row[0]
-        result['uavailability'] = row[1]
-        result['uid'] = row[2]
-        result['tid'] = row[3]
-        result['usday'] = row[4]
+        result['uid'] = row[1]
+        result['tid'] = row[2]
+        result['usday'] = row[3]
         return result
 
-    def build_attr_dict(self, usid, uavailability, uid, tid, usday):
+    def build_attr_dict(self, usid, uid, tid, usday):
         result = {}
         result['usid'] = usid
-        result['uavailability'] = uavailability
         result['uid'] = uid
         result['tid'] = tid
         result['usday'] = usday
@@ -40,24 +38,22 @@ class BaseUserSchedule:
             return jsonify(result), 200
 
     def addNewUserSchedule(self, json):
-        uavailability = json['uavailability']
         uid = json['uid']
         tid = json['tid']
         usday = json['usday']
         dao = UserScheduleDAO()
-        usid = dao.insertUserSchedule(uavailability, uid, tid, usday)
-        result = self.build_attr_dict(usid, uavailability, uid, tid, usday)
+        usid = dao.insertUserSchedule(uid, tid, usday)
+        result = self.build_attr_dict(usid, uid, tid, usday)
         return jsonify(result), 201
 
     def updateUserSchedule(self, json):
-        uavailability = json['uavailability']
         uid = json['uid']
         tid = json['tid']
         usid = json['usid']
         usday = json['usday']
         dao = UserScheduleDAO()
-        updated_user_schedule = dao.updateUserSchedule(usid, uavailability, uid, tid, usday)
-        result = self.build_attr_dict(usid, uavailability, uid, tid, usday)
+        updated_user_schedule = dao.updateUserSchedule(usid, uid, tid, usday)
+        result = self.build_attr_dict(usid, uid, tid, usday)
         return jsonify(result), 200
 
     def deleteUserSchedule(self, usid):
