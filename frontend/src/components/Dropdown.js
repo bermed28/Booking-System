f5a7837e8175edba171a8b592cd50fb3ee95311a
Chris36021 <chris36021@gmail.com>
2021-11-19 20:38:20 -0400

    Finalized Navbar.

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -5,13 +5,17 @@ import "./Dropdown.css"
 import { Item } from 'semantic-ui-react'
 
 const Dropdown = () => {
+    const [dropdown, setDropdown] = useState(false);
+
     return (
         <>
             <ul className="stats-submenu">
                 {statsDropdown.map(item => {
                     return (
                     <li key={item.id}>
-                        <Link to={item.path} className={item.cName}>{item.title}</Link>
+                        <Link to={item.path} className={item.cName}>
+                            {item.title}
+                        </Link>
                     </li>)
                 })}
             </ul>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom"
 import * as Icons from "react-icons/fa"
 import "./Navbar.css";
@@ -7,13 +7,25 @@ import Button from './Button';
 import Dropdown from "./Dropdown"
 
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(false);
+
     return (
         <>
             <nav className="navbar">
                 <Link to="/" className="navbar-logo"><Icons.FaCalendarCheck/> Calendearly</Link>
                 <ul className="nav-items">
                     {navItems.map(item => {
-                        return (<li key={item.id} className={item.cName}>
+                        if (item.title === "Statistics") {
+                            return (
+                                <li key={item.id} className={item.cName} onMouseEnter={() => setDropdown(true)} 
+                                onMouseLeave={() => setDropdown(false)}>
+                                    <Link to={item.path}>{item.title}</Link>
+                                    {/* {dropdown && <Dropdown/>} */}
+                                </li>
+                            )
+                        }
+                        return (
+                        <li key={item.id} className={item.cName}>
                             <Link to={item.path}>{item.title}</Link>
                         </li>
                     )})}
@@ -23,9 +35,8 @@ const Navbar = () => {
                     <Button text="Sign In"/>
                 </div>
             </nav>
-            {/* <Dropdown/> */}
         </>
-    )
+    );
 }
 
 export default Navbar
