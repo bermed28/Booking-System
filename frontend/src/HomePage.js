eb95ee3d26afb5cf8644ba7a73bfcc6c45739afd
Gabriel Diaz Morro <gabriel.diaz34@upr.edu>
2021-11-21 16:04:29 -0400

    Habilitated sign in so it can receive input and save it.

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,19 +1,52 @@
-import React, {Component, useState} from 'react';
-import {Button, Divider, Form, Grid, Header, Modal, Segment, Tab} from 'semantic-ui-react';
+import React, {Component, useRef, useState} from 'react';
+import {Button, Divider, Form, Grid, GridColumn, Header, Modal, Segment, Tab} from 'semantic-ui-react';
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 
 
 function HomePage() {
+
     const [open, setOpen] = useState(false);
     console.log(open);
     const handleChange = (event, newValue) => {
         setOpen(true);
     }
 
+
+    const Foo = () => {
+        const emailInput = useRef(null);
+        const ageInput = useRef(null);
+
+        const handleSubmit = (event) => {
+            event.preventDefault();
+
+            const emailInput = event.target.email; // accessing directly
+            const password = event.target.elements.password; // accessing via `form.elements`
+
+            console.log(emailInput.value); // output: 'myemail@mail.com'
+            console.log(password.value); // output: 'password'
+            getDataAxios()
+
+        };
+        async function getDataAxios(){
+            const response = await axios.get("https://booking-system-stackoverflower.herokuapp.com/StackOverflowersStudios/users")
+            console.log(response.data)
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <Form.Input icon='user' iconPosition='left' type="email" ref={emailInput} name="email" defaultValue="myemail@mail.com" label='Username' />
+                <br/>
+                <Form.Input icon='lock'  iconPosition='left'type="text" ref={ageInput} name="password" defaultValue="password" label='Password'/>
+                <br/>
+                <Button type="submit" content='Login' primary />
+            </form>
+        );
+    };
     return (
-        <>
-            <Navbar></Navbar>
+         <>
+            <Navbar/>
             <Segment><Header dividing textAlign="center" size="huge">Sign Up for Calendearly</Header>
         <Modal centered={false} open={open} onClose={() => setOpen(false)} onOpen={() => setOpen(true)}>
             <Modal.Header>Needs changing!</Modal.Header>
@@ -30,19 +63,7 @@ function HomePage() {
 
             <Grid columns={2} relaxed='very' stackable>
                 <Grid.Column>
-                    <Form>
-                        <Form.Input
-                            icon='user'
-                            iconPosition='left'
-                            label='Username'
-                            placeholder='Username' />
-                        <Form.Input
-                            icon='lock'
-                            iconPosition='left'
-                            label='Password'
-                            type='password' />
-                        <Button content='Login' primary onClick={handleChange} />
-                    </Form>
+                    {Foo()}
                 </Grid.Column>
                 <Grid.Column verticalAlign='middle'>
                     <Button content='Sign up' icon='signup' size='big' onClick={handleChange} />
@@ -53,8 +74,8 @@ function HomePage() {
         </Segment>
         </Segment>
         </>
-    )
-}
+        )
+    }
 
 
 export default HomePage;
