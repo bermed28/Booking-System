48196ae754c9250421d9c75c0b79adfebc69f7d3
Chris36021 <chris36021@gmail.com>
2021-12-02 00:09:25 -0400

    Fixed visual glitch in landing page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,26 +20,26 @@ const App = () => {
         <>
             <Navbar/>
             <Segment placeholder>
-            <Grid columns={2} relaxed='very' stackable>
-                <Grid.Column>
-                    <div className="container">
-                        <img alt="Calendar Picture" src={require("./Assets/mini-calendar-2022-printable-planner-cards-04-01.jpg").default}/>
-                    </div>
-                </Grid.Column>
-                <Grid.Column verticalAlign='middle'>
-                    <div className="container" style={{textAlign: "center", verticalAlign: "baseline"}}>
-                        <h1>A New Way to Meet</h1>
-                        <p>Set up important meetings and reservations with your colleagues, <br/> or simply reserve a space to meet
-                            with your friends and loved ones. <br/> All from one place.
-                        </p>
-                        <Link to="Signup">
+                <Grid columns={2} relaxed='very' stackable>
+                    <Grid.Column>
+                        <div className="container">
+                            <img alt="Calendar Picture" src={require("./Assets/mini-calendar-2022-printable-planner-cards-04-01.jpg").default}/>
+                        </div>
+                    </Grid.Column>
+                    <Grid.Column verticalAlign='middle'>
+                        <div className="container" style={{border: '1px solid rgba(0, 0, 0, 0.05)', textAlign: "center", verticalAlign: "baseline", background: "white", borderWidth: 2, borderColor: "black", padding: 3}}>
+                            <h1>A New Way to Meet</h1>
+                            <p>Set up important meetings and reservations with your colleagues, <br/> or simply reserve a space to meet
+                                with your friends and loved ones. <br/> All from one place.
+                            </p>
+                            <Link to="Signup">
                                 <Button content='Get started' icon='calendar' size='big' color="violet" />
-                        </Link>
-                    </div>
-                </Grid.Column>
-            </Grid>
-            <Divider> </Divider>
-        </Segment>
+                            </Link>
+                        </div>
+                    </Grid.Column>
+                </Grid>
+                <Divider> </Divider>
+            </Segment>
         </>
     );
 }
